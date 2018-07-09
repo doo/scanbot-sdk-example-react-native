1a0c988f39296906a19e5c130f38d4ca525d3a94
Eduard Frank <eddy@scanbot.io>
2018-07-09 16:16:09 +0200

    Testing/preparing first beta release

--- Screens/index.js
+++ Screens/index.js
@@ -2,7 +2,6 @@ import { Navigation, ScreenVisibilityListener } from 'react-native-navigation';
 
 import MainScreen from './MainScreen.js';
 import CameraKitGalleryDemoScreen from './CameraKitGalleryDemoScreen';
-import ReviewScreen from './ReviewScreen';
 
 export const DemoScreens = {
   MainScreen: {
@@ -14,37 +13,15 @@ export const DemoScreens = {
     id: 'myDemo.CameraKitGalleryDemoScreen',
     title: 'Select an image'
   },
-
-  ReviewScreen: {
-    id: 'myDemo.ReviewScreen',
-    title: 'Stored Pages',
-  }
 };
-let licenseKey =
-  "M+NzOkvq5IioisKLbzwGTFozoltOAM" +
-  "6bMRIxxfikeErKtt8XFPN70rFkCctd" +
-  "U5GTUeJvJKmqj6BRHYSaMJyDNWQ7Kq" +
-  "t5XUQGuhPbP2JqWvPBQ1wMg4hv4dte" +
-  "czpKJuMxNmwkwMK1c4aozdzZdLsB4L" +
-  "A7wQeWiBCU7Vdbz7PhJPR3nYpXoT/6" +
-  "mBSo/hyEdB4Y+tZqfJd82JMCo7Lp5q" +
-  "e6uq8ZSti3rjU48zrbe8zX+2g2UXSi" +
-  "JzRiCPOxxJMLjmqmI/R6sxvmbM0eLK" +
-  "SVxFGTlHMCAHRaS4H7YzorgqT7aUtj" +
-  "8WwgoS9b+NjgpGeyUw6r4L9FmO4I14" +
-  "S0a/NfyeZOow==\nU2NhbmJvdFNESw" +
-  "ppby5zY2FuYm90LmRlbW8ucmVhY3Ru" +
-  "YXRpdmUKMTUzMDc0ODc5OQozMjc2Nw" +
-  "oz\n";
 
 export const DemoConstants = {
-  scanbotLicenseKey: licenseKey
+  scanbotLicenseKey: '' // Add the Scanbot SDK license key string here
 };
 
 export function registerScreens() {
   Navigation.registerComponent(DemoScreens.MainScreen.id, () => MainScreen);
   Navigation.registerComponent(DemoScreens.CameraKitGalleryDemoScreen.id, () => CameraKitGalleryDemoScreen);
-  Navigation.registerComponent(DemoScreens.ReviewScreen.id, () => ReviewScreen);
 }
 
 export function registerScreenVisibilityListener() {
