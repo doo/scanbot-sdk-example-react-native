a0af2f4819912e6174531f2b243fb3898e1085a1
Rutkay Biyik <rutkay@scanbot.io>
2022-01-13 13:18:52 +0100

    v4.11.3-alpha3

--- src/pages/ImageDetailScreen.tsx
+++ src/pages/ImageDetailScreen.tsx
@@ -104,7 +104,7 @@ export class ImageDetailScreen extends BaseScreen {
         doneButtonTitle: 'Apply',
         topBarBackgroundColor: Colors.SCANBOT_RED,
         bottomBarBackgroundColor: Colors.SCANBOT_RED,
-        interfaceOrientation: 'PORTRAIT',
+        interfaceOrientation: 'ALL',
         // See further config properties ...
       },
     );
