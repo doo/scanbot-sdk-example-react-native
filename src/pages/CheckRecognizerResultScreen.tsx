4b920329a56942cbbcb5fd9092b66af80a70f02f
Marco <marco@scanbot.io>
2023-06-13 15:12:47 +0200

    v4.15.0-beta.1

--- src/pages/CheckRecognizerResultScreen.tsx
+++ src/pages/CheckRecognizerResultScreen.tsx
@@ -65,7 +65,6 @@ const getCheckFields = (checkResult: CheckRecognizerResult) => {
       return undefined;
     }
     const text = value.text;
-    const confidence = value.confidence;
 
     if (!text) {
       return undefined;
@@ -73,7 +72,7 @@ const getCheckFields = (checkResult: CheckRecognizerResult) => {
 
     return JSON.stringify({
       key: key,
-      value: `${text} (confidence: ${confidence})`,
+      value: text,
     });
   });
 };
