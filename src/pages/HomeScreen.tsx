2f366cc9fa899c1f6110662d398f1ff587ab218c
StefanKScanbot <stefankrzhovski@scanbot.io>
2023-08-30 12:13:51 +0200

    ImageFilterType adjustments

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -11,31 +11,31 @@ import {
   View,
 } from 'react-native';
 import ScanbotSDK, {
-  BarcodeScannerConfiguration,
-  DocumentScannerConfiguration,
-  MrzScannerConfiguration,
-  LicensePlateScannerConfiguration,
-  MedicalCertificateRecognizerConfiguration,
-  TextDataScannerConfiguration,
-  BatchBarcodeScannerConfiguration,
-  HealthInsuranceCardScannerConfiguration,
-  GenericDocumentRecognizerResult,
-  MedicalCertificateScannerResult,
-  BarcodeResultField,
-  BaseDocumentFormat,
   AAMVADocumentFormat,
+  BarcodeResultField,
+  BarcodeScannerConfiguration,
+  BaseDocumentFormat,
+  BatchBarcodeScannerConfiguration,
   BoardingPassDocumentFormat,
-  MedicalPlanDocumentFormat,
-  MedicalCertificateDocumentFormat,
+  CheckRecognizerConfiguration,
+  DocumentScannerConfiguration,
+  GenericDocumentRecognizerConfiguration,
+  GenericDocumentRecognizerResult,
+  GS1DocumentFormat,
+  HealthInsuranceCardScannerConfiguration,
   IDCardPDF417DocumentFormat,
+  ImageFilterType,
+  LicensePlateScannerConfiguration,
+  LicensePlateScanStrategy,
+  MedicalCertificateDocumentFormat,
+  MedicalCertificateRecognizerConfiguration,
+  MedicalCertificateScannerResult,
+  MedicalPlanDocumentFormat,
+  MrzScannerConfiguration,
   SEPADocumentFormat,
   SwissQRCodeDocumentFormat,
+  TextDataScannerConfiguration,
   VCardDocumentFormat,
-  GS1DocumentFormat,
-  GenericDocumentRecognizerConfiguration,
-  LicensePlateScanStrategy,
-  CheckRecognizerConfiguration,
-  ImageFilter,
 } from 'react-native-scanbot-sdk';
 // @ts-ignore
 import {ActionSheetCustom as ActionSheet} from 'react-native-custom-actionsheet';
@@ -60,7 +60,7 @@ const CANCEL_INDEX = 0;
 
 export class HomeScreen extends BaseScreen {
   filterActionSheet: any;
-  onFilterSelected?: (filter: ImageFilter) => void;
+  onFilterSelected?: (filter: ImageFilterType) => void;
   getFilterActionSheetRef = (ref: any) => (this.filterActionSheet = ref);
   handleActionSheetPress = async (index: number) => {
     const filter = SDKUtils.IMAGE_FILTERS[index];
@@ -677,7 +677,7 @@ export class HomeScreen extends BaseScreen {
       return;
     }
 
-    this.onFilterSelected = async (filter: ImageFilter) => {
+    this.onFilterSelected = async (filter: ImageFilterType) => {
       this.showProgress();
       const result = await ScanbotSDK.applyImageFilter(uri, filter);
       const filteredImageUri = result.imageFileUri;
