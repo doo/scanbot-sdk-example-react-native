62a467b3a88ecaf020dc637d8b26d705b7a56825
StefanKScanbot <stefankrzhovski@scanbot.io>
2023-09-14 09:47:48 +0200

    Updated packages and optimized clean scripts

--- RN-0.71.X/src/utils/PageStorage.ts
+++ RN-0.71.X/src/utils/PageStorage.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Page} from 'react-native-scanbot-sdk';
 
 export class PageStorage {
