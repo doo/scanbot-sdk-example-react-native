a055a1cd250eb3a7278d37b6b8e686a375c8725d
StefanKScanbot <stefankrzhovski@scanbot.io>
2023-09-14 11:59:52 +0200

    Upgraded react-native framework to 0.72.4

--- RN-0.71.X/__tests__/App.test.tsx
+++ RN-0.71.X/__tests__/App.test.tsx
@@ -9,6 +9,9 @@ import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// Note: import explicitly to use the types shiped with jest.
+import {it} from '@jest/globals';
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
