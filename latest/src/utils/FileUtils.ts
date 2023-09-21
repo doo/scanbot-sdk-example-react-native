92d350bc00f497c4cb65fde005135286efba82ea
StefanKScanbot <stefankrzhovski@scanbot.io>
2023-09-21 12:29:03 +0200

    refactored FileUtils.ts

--- latest/src/utils/FileUtils.ts
+++ latest/src/utils/FileUtils.ts
@@ -6,23 +6,21 @@ export interface PickSingleFileResult {
   error?: string;
 }
 
-export class FileUtils {
-  static async pickPdf(): Promise<PickSingleFileResult> {
-    try {
-      const res = await DocumentPicker.pickSingle({
-        type: [DocumentPicker.types.pdf],
-      });
-      return {
-        status: 'OK',
-        fileUrl: res.uri,
-      };
-    } catch (err) {
-      const error = err as Error & {code?: string | undefined};
-      if (DocumentPicker.isCancel(error)) {
-        return {status: 'CANCELED'};
-      } else {
-        return {status: 'ERROR', error: error.code};
-      }
+export async function pickPdf(): Promise<PickSingleFileResult> {
+  try {
+    const res = await DocumentPicker.pickSingle({
+      type: [DocumentPicker.types.pdf],
+    });
+    return {
+      status: 'OK',
+      fileUrl: res.uri,
+    };
+  } catch (err) {
+    const error = err as Error & {code?: string | undefined};
+    if (DocumentPicker.isCancel(error)) {
+      return {status: 'CANCELED'};
+    } else {
+      return {status: 'ERROR', error: error.code};
     }
   }
 }
