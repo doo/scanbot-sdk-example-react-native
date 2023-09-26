2cea6c5405f9b2144573ce4a2b8a7885b64604a0
StefanKScanbot <stefankrzhovski@scanbot.io>
2023-09-26 14:21:01 +0200

    refactored styles to ImageResultScreen.tsx

--- latest/src/pages/ImageResultScreen.tsx
+++ latest/src/pages/ImageResultScreen.tsx
@@ -1,7 +1,11 @@
 import React, {useCallback, useContext, useState} from 'react';
-import {SafeAreaView, TouchableOpacity, View} from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import ScanbotSDK, {Page} from 'react-native-scanbot-sdk/src';
-import {Styles} from '../model/Styles';
 import PreviewImage from '../ui/PreviewImage';
 import {ScrollView} from 'react-native-gesture-handler';
 import {PageContext} from '../context/usePages';
@@ -11,20 +15,22 @@ import {useDocumentScanner} from '../hooks/examples/useDocumentScanner';
 import {errorMessageAlert, infoMessageAlert} from '../utils/Alerts';
 import {SavePagesModal} from '../components/SavePagesModal';
 import {useNavigation} from '@react-navigation/native';
+import {COLORS} from '../theme/Theme';
+
+const GALLERY_CELL_PADDING = 20;
 
 export function ImageResultScreen() {
   const navigation = useNavigation<PrimaryRouteNavigationProp>();
-  const {pageList, deleteAllPages, isPageListEmpty, addSelectedPage} =
-    useContext(PageContext);
+  const {pageList, deleteAllPages, isPageListEmpty} = useContext(PageContext);
   const [modalVisible, setModalVisible] = useState(false);
   const documentScanner = useDocumentScanner();
+  const dimensions = useWindowDimensions();
 
   const onSelect = useCallback(
     (page: Page) => {
-      addSelectedPage(page);
-      navigation.navigate(Screens.IMAGE_DETAILS);
+      navigation.navigate(Screens.IMAGE_DETAILS, page);
     },
-    [addSelectedPage, navigation],
+    [navigation],
   );
 
   const onSave = useCallback(() => {
@@ -52,16 +58,20 @@ export function ImageResultScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={Styles.INSTANCE.imageResults.container}>
-      <ScrollView style={Styles.INSTANCE.imageResults.scrollView}>
-        <View style={Styles.INSTANCE.imageResults.gallery}>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.gallery}>
           {pageList.map(page => (
             <TouchableOpacity onPress={() => onSelect(page)} key={page.pageId}>
               <PreviewImage
                 imageUri={page.documentPreviewImageFileUri}
                 style={[
-                  Styles.INSTANCE.imageResults.galleryCell,
-                  Styles.INSTANCE.common.containImage,
+                  styles.galleryCell,
+                  styles.containImage,
+                  {
+                    width: (dimensions.width - 4 * GALLERY_CELL_PADDING) / 3,
+                    height: (dimensions.width - 4 * GALLERY_CELL_PADDING) / 3,
+                  },
                 ]}
               />
             </TouchableOpacity>
@@ -69,11 +79,36 @@ export function ImageResultScreen() {
         </View>
       </ScrollView>
       <BottomActionBar
-        onAdd={documentScanner}
-        onSave={onSave}
-        onDelete={onDelete}
+        buttonOneTitle={'ADD'}
+        buttonTwoTitle={'SAVE'}
+        buttonThreeTitle={'DELETE ALL'}
+        onButtonOne={documentScanner}
+        onButtonTwo={onSave}
+        onButtonThree={onDelete}
       />
       <SavePagesModal onDismiss={onDismiss} isVisible={modalVisible} />
-    </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gallery: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingBottom: 64,
+  },
+  galleryCell: {
+    marginLeft: GALLERY_CELL_PADDING,
+    marginTop: GALLERY_CELL_PADDING,
+    backgroundColor: COLORS.LIGHT_GRAY,
+  },
+  containImage: {
+    resizeMode: 'contain',
+  },
+});
