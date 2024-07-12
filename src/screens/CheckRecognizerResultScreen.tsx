9b229241d3fc8846525cff686e20ca0a6ea4906e
StefanKScanbot <stefankrzhovski@scanbot.io>
2024-07-12 14:49:28 +0200

    Refactored check result

--- src/screens/CheckRecognizerResultScreen.tsx
+++ src/screens/CheckRecognizerResultScreen.tsx
@@ -1,42 +1,63 @@
-import React from 'react';
-import {ScanResultSectionList} from '../components/ScanResultSectionList';
+import React, {useMemo} from 'react';
 import {useRoute} from '@react-navigation/native';
 import {CheckRecognizerResultScreenRouteProp} from '../utils/Navigation';
 import {GenericDocumentUtils} from '../utils/GenericDocumentUtils';
-import {CheckRecognizerResult} from 'react-native-scanbot-sdk';
+import {ResultContainer} from '../components/ResultContainer';
+import {ResultHeader} from '../components/ResultHeader';
+import {ResultFieldRow} from '../components/ResultFieldRow';
+import {StyleSheet, View} from 'react-native';
+import {PreviewImage} from '../components/PreviewImage';
 
 export function CheckRecognizerResultScreen() {
   const {params: checkRecognizerResult} =
     useRoute<CheckRecognizerResultScreenRouteProp>();
 
+  const CheckDocument = useMemo(() => {
+    if (!checkRecognizerResult.check) {
+      return () => null;
+    }
+    return () => (
+      <View>
+        <ResultHeader title={'Check Document Result'} />
+        {GenericDocumentUtils.extractGenericDocumentFields(
+          checkRecognizerResult.check,
+        ).map((field, index) => (
+          <ResultFieldRow
+            key={field.type.name + index}
+            title={field.type.name.trim()}
+            value={field.value?.text}
+          />
+        ))}
+      </View>
+    );
+  }, [checkRecognizerResult]);
+
   return (
-    <ScanResultSectionList
-      sectionData={[
-        {
-          title: 'Check Result',
-          data: checkResultData(checkRecognizerResult),
-        },
-        {
-          title: checkRecognizerResult.check?.type.name ?? 'NOT DETECTED',
-          data: GenericDocumentUtils.gdrFields(checkRecognizerResult.check),
-        },
-      ]}
-    />
+    <ResultContainer>
+      <PreviewImage
+        imageUri={checkRecognizerResult.imageFileUri}
+        style={[styles.image]}
+      />
+      <ResultHeader title={'Check recognition'} />
+      <ResultFieldRow
+        title={'Check status'}
+        value={checkRecognizerResult.checkStatus}
+      />
+      <ResultFieldRow
+        title={'Check type'}
+        value={checkRecognizerResult.checkType}
+      />
+      <CheckDocument />
+    </ResultContainer>
   );
 }
 
-const checkResultData = (checkRecognizerResult: CheckRecognizerResult) => [
-  GenericDocumentUtils.sectionImageItem(
-    'Check Image',
-    checkRecognizerResult.imageFileUri,
-  ),
-  GenericDocumentUtils.sectionValueItem(
-    'Recognition Status',
-    checkRecognizerResult.checkStatus,
-  ),
-  GenericDocumentUtils.sectionValueItem(
-    'Check Type',
-    checkRecognizerResult.checkType,
-  ),
-  ...GenericDocumentUtils.gdrCommonFields(checkRecognizerResult.check),
-];
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    height: 150,
+    resizeMode: 'contain',
+    backgroundColor: '#222222',
+    margin: 16,
+  },
+});
