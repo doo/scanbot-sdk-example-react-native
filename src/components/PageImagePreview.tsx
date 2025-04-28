ac5affe430180063a604cb2f45513b97d8c0218e
StefanKScanbot <stefankrzhovski@scanbot.io>
2025-04-28 12:26:00 +0200

    Fixed Document Results when encryption is enabled

--- src/components/PageImagePreview.tsx
+++ src/components/PageImagePreview.tsx
@@ -1,7 +1,8 @@
-import {PageData} from 'react-native-scanbot-sdk';
+import ScanbotSDK, {PageData} from 'react-native-scanbot-sdk';
 import {ImageStyle, StyleProp} from 'react-native';
-import React, {useMemo} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {PreviewImage} from './PreviewImage.tsx';
+import {FILE_ENCRYPTION_ENABLED, IMAGE_FILE_FORMAT} from '@utils';
 
 export function PageImagePreview({
   page,
@@ -10,6 +11,35 @@ export function PageImagePreview({
   page: PageData;
   style: StyleProp<ImageStyle>;
 }) {
+  const [uri, setUri] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadDecryptedImageData() {
+      try {
+        setLoading(true);
+        if (page.documentImageURI != null) {
+          const result = await ScanbotSDK.getImageData(page.documentImageURI);
+          const imgMimeType =
+            IMAGE_FILE_FORMAT === 'JPG' ? 'image/jpeg' : 'image/png';
+          setUri(`data:${imgMimeType};base64,${result.base64ImageData}`);
+        }
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (FILE_ENCRYPTION_ENABLED) {
+      // File encryption is enabled, so we need to load the decrypted image data
+      // as base64 from SDK. The SDK decrypts the image data under the hood.
+      loadDecryptedImageData();
+    } else {
+      setUri(`${page.documentImageURI}?=${Date.now()}`);
+    }
+  }, [page.documentImageURI, setLoading]);
+
   const pageImageKey = useMemo(() => {
     return `${page?.uuid}_${Date.now()}`;
   }, [page]);
@@ -17,7 +47,8 @@ export function PageImagePreview({
   return (
     <PreviewImage
       key={pageImageKey}
-      imageSource={page.documentImageURI ?? page.originalImageURI}
+      loading={loading}
+      imageSource={uri}
       style={style}
     />
   );
