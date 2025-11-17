d2f3293b55d9c689467352ea1004857b709bd5c9
StefanKScanbot <stefankrzhovski@scanbot.io>
2025-11-17 16:21:45 +0100

    Update text pattern code snippets

---
+++ src/codeSnippets/text-pattern/finder-overlay.ts
@@ -0,0 +1,34 @@
+import {
+  FinderCorneredStyle,
+  FinderStrokedStyle,
+  startTextPatternScanner,
+  TextPatternScannerScreenConfiguration,
+} from 'react-native-scanbot-sdk/ui_v2';
+
+async function startScanning() {
+  try {
+    /** Create an instance of the default configuration */
+    const configuration = new TextPatternScannerScreenConfiguration();
+    /** Retrieve the instance of the viewFinder from the configuration object. */
+    const viewFinder = configuration.viewFinder;
+    /**
+     * Configure the view finder.
+     * Choose between cornered or stroked style.
+     */
+    viewFinder.style = new FinderStrokedStyle();
+    /** For default cornered style. */
+    viewFinder.style = new FinderCorneredStyle();
+    /** You can also set each style's stroke width, stroke color or corner radius. */
+    viewFinder.style = new FinderCorneredStyle({
+      strokeWidth: 3,
+      strokeColor: '#ff0000',
+    });
+    /** Start the Text Pattern Scanner **/
+    const textPatternResult = await startTextPatternScanner(configuration);
+    /** Handle the result if the status is 'OK' */
+    if (textPatternResult.status === 'OK') {
+    }
+  } catch (e: any) {
+    console.error(e.message);
+  }
+}
